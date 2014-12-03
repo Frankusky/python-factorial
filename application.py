9f1282bdb29246d14f00667f3d08d5e0e75d5b44
Frank Sam Hu <fsh1991@hotmail.com>
2014-12-03 09:05:52 -0600

    First commit

--- application.py
+++ application.py
@@ -1,3 +1,15 @@
-# Aqui escribe tu codigo
+# -*- coding: UTF-8 -*-
+def factorial(x,n):
+	if(n>0):
+		x=factorial(x,n-1)
+		x=x*n
+	else:
+		x=1
+	return x
 
-print "Hello World"
+try: 
+	numero = int(raw_input("inserta un numero "))
+	calculo=factorial(1,numero)
+	print "El factorial de %s es %s" % (numero,calculo)
+except:
+	print "\nTiene que ser un valor numerico"
